fe51b27d3adc26960a78c35b0071ca7d3f90f4a8
atharva-boop <atharvaverma20194080@gmail.com>
2020-10-07 22:24:38 +0530

    sdsdf

--- LockDownTraveller/Login.java
+++ LockDownTraveller/Login.java
@@ -1,111 +1,96 @@
 package LockDownTraveller;
 
-
 import java.awt.*;
 import javax.swing.*;
 
 public class Login extends JFrame {
-    JLabel l1,l2,l3;
+    JLabel l1, l2, l3;
     JTextField tf1;
     JPasswordField pf2;
-    JButton b1,b2,b3;
-  
-    Login(){
-         
-        
-        //Move the text to the center
-        
+    JButton b1, b2, b3;
+
+    Login() {
+
+        // Move the text to the center
+
         setFont(new Font("System", Font.BOLD, 22));
         Font f = getFont();
         FontMetrics fm = getFontMetrics(f);
         int x = fm.stringWidth("LOGIN DIALOG BOX");
         int y = fm.stringWidth(" ");
         int z = getWidth() - x;
-        int w = z/y;
-        String pad ="";
-        //for (int i=0; i!=w; i++) pad +=" ";   
-        pad = String.format("%"+w+"s", pad);
-        setTitle(pad+"LOGIN DIALOG BOX");
-        
-       
-       
-        
-        
-        
+        int w = z / y;
+        String pad = "";
+        // for (int i=0; i!=w; i++) pad +=" ";
+        pad = String.format("%" + w + "s", pad);
+        setTitle(pad + "LOGIN DIALOG BOX");
+
         l1 = new JLabel("LOGIN");
         l1.setFont(new Font("Osward", Font.BOLD, 38));
-        
+
         l2 = new JLabel("USER ID:");
         l2.setFont(new Font("Raleway", Font.BOLD, 28));
-        
+
         l3 = new JLabel("PASSWORD:");
         l3.setFont(new Font("Raleway", Font.BOLD, 28));
-        
+
         tf1 = new JTextField(15);
         pf2 = new JPasswordField(15);
-        
+
         b1 = new JButton("LOG IN");
         b1.setBackground(Color.BLACK);
         b1.setForeground(Color.WHITE);
-        
+
         b2 = new JButton("CLEAR");
         b2.setBackground(Color.BLACK);
         b2.setForeground(Color.WHITE);
-        
+
         b3 = new JButton("SIGN UP");
         b3.setBackground(Color.BLACK);
         b3.setForeground(Color.WHITE);
-        
-        setLayout(null);
-        
-        l1.setBounds(175,50,450,200);
-        add(l1);
-        
-        l2.setBounds(125,150,375,200);
-        add(l2);
-        
-        tf1.setBounds(300,235,230,30);
-        add(tf1);
-        
-        l3.setBounds(125,225,375,200);
-        add(l3);
-        
-        tf1.setFont(new Font("Arial", Font.BOLD, 14));
-       
-        
-        pf2.setFont(new Font("Arial", Font.BOLD, 14));
-        pf2.setBounds(300,310,230,30);
-        add(pf2);
-        
-        b1.setFont(new Font("Arial", Font.BOLD, 14));
-        b1.setBounds(300,400,100,30);
-        add(b1);
-        
-        b2.setFont(new Font("Arial", Font.BOLD, 14));
-        b2.setBounds(430,400,100,30);
-        add(b2);
-        
-        b3.setFont(new Font("Arial", Font.BOLD, 14));
-        b3.setBounds(300,450,230,30);
-        add(b3);
-        
-        
-      
-        
-        getContentPane().setBackground(Color.WHITE);
-        
-        setSize(600,600);
-        setLocation(100,100);
-        setVisible(true);
-        
 
-        
+        setLayout(null);
+
+        l1.setBounds(175, 50, 450, 200);
+        add(l1);
+
+        l2.setBounds(125, 150, 375, 200);
+        add(l2);
+
+        tf1.setBounds(300, 235, 230, 30);
+        add(tf1);
+
+        l3.setBounds(125, 225, 375, 200);
+        add(l3);
+
+        tf1.setFont(new Font("Arial", Font.BOLD, 14));
+
+        pf2.setFont(new Font("Arial", Font.BOLD, 14));
+        pf2.setBounds(300, 310, 230, 30);
+        add(pf2);
+
+        b1.setFont(new Font("Arial", Font.BOLD, 14));
+        b1.setBounds(300, 400, 100, 30);
+        add(b1);
+
+        b2.setFont(new Font("Arial", Font.BOLD, 14));
+        b2.setBounds(430, 400, 100, 30);
+        add(b2);
+
+        b3.setFont(new Font("Arial", Font.BOLD, 14));
+        b3.setBounds(300, 450, 230, 30);
+        add(b3);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        setSize(600, 600);
+        setLocation(100, 100);
+        setVisible(true);
+
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Login().setVisible(true);
     }
 
 }
-
-    
