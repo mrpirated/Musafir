7985e1d37549c2026a9b225c253900d118ed9278
Aditya Darji <darjiaditya2000@gmail.com>
2020-10-22 18:30:59 +0530

    loginAdmin server connected

--- Musafir/LoginAdmin.java
+++ Musafir/LoginAdmin.java
@@ -2,8 +2,11 @@ package Musafir;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import Classes.*;
 
-public class LoginAdmin extends JFrame {
+public class LoginAdmin extends JFrame implements ActionListener {
     JLabel l1, l2, l3;
     JTextField tf1;
     JPasswordField pf2;
@@ -22,7 +25,7 @@ public class LoginAdmin extends JFrame {
         int w = z / y;
         String pad = "";
         // for (int i=0; i!=w; i++) pad +=" ";
-        pad = String.format("%" + w*0.8 + "s", pad);
+        pad = String.format("%" + w * 0.8 + "s", pad);
         setTitle(pad + "ADMIN LOGIN DIALOG BOX");
 
         l1 = new JLabel("ADMIN LOGIN");
@@ -73,11 +76,51 @@ public class LoginAdmin extends JFrame {
         b2.setBounds(275, 225, 200, 30);
         add(b2);
 
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+
         getContentPane().setBackground(Color.WHITE);
 
         setSize(500, 400);
         setLocation(500, 250);
         setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        try {
+
+            if (ae.getSource() == b1) {
+                String username = tf1.getText();
+                char[] password = pf2.getPassword();
+                LoginInfo admin = new LoginInfo(username, password);
+                try {
+
+                    ObjectOutputStream os = new ObjectOutputStream(Connect.socket.getOutputStream());
+                    os.writeInt(4);
+                    os.writeObject(admin);
+                    os.flush();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ObjectInputStream oi = new ObjectInputStream(Connect.socket.getInputStream());
+                String s = (String) oi.readUTF();
+                if (s.equals("")) {
+
+                    JOptionPane.showMessageDialog(null, "Username or Password entered is Incorrect");
+                } else {
+                    new AdminHome().setVisible(true);
+                    setVisible(false);
+                }
+
+            } else if (ae.getSource() == b2) {
+                tf1.setText("");
+                pf2.setText("");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
--- MusafirServer/HandleClient.java
+++ MusafirServer/HandleClient.java
@@ -5,111 +5,138 @@ import java.net.*;
 import java.sql.*;
 
 import Classes.*;
-public class HandleClient implements Runnable{
-    private Conn c1=new Conn();
+
+public class HandleClient implements Runnable {
+    private Conn c1 = new Conn();
     private Socket socket;
-    public HandleClient(Socket socket){
-        this.socket=socket;
+
+    public HandleClient(Socket socket) {
+        this.socket = socket;
     }
 
-    private String Login(LoginInfo loginInfo){
-        try{
-        String query="SELECT * FROM `user_info` WHERE email='"+loginInfo.getUsername()+"' AND password='"+String.valueOf(loginInfo.getPassword())+"'";
-        
-            ResultSet rs=c1.s.executeQuery(query);
-            if(rs.next())
-            {
+    private String Login(LoginInfo loginInfo) {
+        try {
+            String query = "SELECT * FROM `user_info` WHERE email='" + loginInfo.getUsername() + "' AND password='"
+                    + String.valueOf(loginInfo.getPassword()) + "'";
+
+            ResultSet rs = c1.s.executeQuery(query);
+            if (rs.next()) {
                 return rs.getString("name");
             }
-        query="SELECT * FROM `user_info` WHERE phone='"+loginInfo.getUsername()+"' AND password='"+String.valueOf(loginInfo.getPassword())+"'";
-            rs=c1.s.executeQuery(query);
-            if(rs.next())
-            {
+            query = "SELECT * FROM `user_info` WHERE phone='" + loginInfo.getUsername() + "' AND password='"
+                    + String.valueOf(loginInfo.getPassword()) + "'";
+            rs = c1.s.executeQuery(query);
+            if (rs.next()) {
                 return rs.getString("name");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-                
+
         }
         return "";
     }
-    private Boolean Signup(UserInfo userInfo){
-        
-        String d=userInfo.getMm()+"-"+userInfo.getDd()+"-"+userInfo.getYy();
-        String query="INSERT INTO `user_info` ( `name`, `dob`, `gender`, `email`, `phone`, `password`) VALUES ( '"+userInfo.getName()+"', STR_TO_DATE('"+d+"','%m-%d-%Y'), '"+userInfo.getGender()+"', '"+userInfo.getEmail()+"', '"+userInfo.getPhone()+"', '"+String.valueOf(userInfo.getPassword())+"')";
-        try{
-        c1.s.executeUpdate(query);
-        }catch(Exception e){
+
+    private Boolean Signup(UserInfo userInfo) {
+
+        String d = userInfo.getMm() + "-" + userInfo.getDd() + "-" + userInfo.getYy();
+        String query = "INSERT INTO `user_info` ( `name`, `dob`, `gender`, `email`, `phone`, `password`) VALUES ( '"
+                + userInfo.getName() + "', STR_TO_DATE('" + d + "','%m-%d-%Y'), '" + userInfo.getGender() + "', '"
+                + userInfo.getEmail() + "', '" + userInfo.getPhone() + "', '" + String.valueOf(userInfo.getPassword())
+                + "')";
+        try {
+            c1.s.executeUpdate(query);
+        } catch (Exception e) {
             e.printStackTrace();
-            
+
         }
 
         return true;
     }
-    private String[][] GetCities(){
-        String[][] cities=new String[2][];
-        String query="SELECT COUNT(*) FROM Cities";
 
-        try{
-            ResultSet rs=c1.s.executeQuery(query);
+    private String[][] GetCities() {
+        String[][] cities = new String[2][];
+        String query = "SELECT COUNT(*) FROM Cities";
+
+        try {
+            ResultSet rs = c1.s.executeQuery(query);
             rs.next();
-            int n=rs.getInt(1);
-            cities[0]= new String[n];
-            cities[1]= new String[n];
-            query="SELECT * FROM Cities";
-            rs=c1.s.executeQuery(query);
-            for(int i=0;i<n;i++){
+            int n = rs.getInt(1);
+            cities[0] = new String[n];
+            cities[1] = new String[n];
+            query = "SELECT * FROM Cities";
+            rs = c1.s.executeQuery(query);
+            for (int i = 0; i < n; i++) {
                 rs.next();
-                cities[0][i]=(String)rs.getString("stations");
-                cities[1][i]=(String)rs.getString("short");
+                cities[0][i] = (String) rs.getString("stations");
+                cities[1][i] = (String) rs.getString("short");
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return cities;
     }
+
+    private String AdminLogin(LoginInfo loginInfo) {
+        try {
+            String query = "SELECT * FROM `admin_info` WHERE email='" + loginInfo.getUsername() + "' AND password='"
+                    + String.valueOf(loginInfo.getPassword()) + "'";
+
+            ResultSet rs = c1.s.executeQuery(query);
+            if (rs.next()) {
+                return rs.getString("name");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return "";
+    }
+
     @Override
     public void run() {
-        while(true){
-            try{
-                
-                ObjectInputStream oi =new ObjectInputStream(socket.getInputStream());
-                
-                ObjectOutputStream os =new ObjectOutputStream(socket.getOutputStream());
-                int choice =(int)oi.readInt();
-                switch(choice){
+        while (true) {
+            try {
+
+                ObjectInputStream oi = new ObjectInputStream(socket.getInputStream());
+
+                ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+                int choice = (int) oi.readInt();
+                switch (choice) {
                     case 1:
-                    LoginInfo loginInfo=(LoginInfo)oi.readObject();
-                    String s= Login(loginInfo);
-                    os.writeUTF(s);
-                    
-                    os.flush();
-                    break;
+                        LoginInfo loginInfo = (LoginInfo) oi.readObject();
+                        String s = Login(loginInfo);
+                        os.writeUTF(s);
+                        os.flush();
+                        break;
 
                     case 2:
-                    UserInfo userInfo=(UserInfo)oi.readObject();
-                    Boolean b =Signup(userInfo);
-                    os.writeBoolean(b);
-                    os.flush();
-                    break;
+                        UserInfo userInfo = (UserInfo) oi.readObject();
+                        Boolean b = Signup(userInfo);
+                        os.writeBoolean(b);
+                        os.flush();
+                        break;
 
                     case 3:
-                    String [][] cities=GetCities();
-                    os.writeObject(cities);
-                    os.flush();
-                    break;
-                     
+                        String[][] cities = GetCities();
+                        os.writeObject(cities);
+                        os.flush();
+                        break;
 
+                    case 4:
+                        LoginInfo adminLogin = (LoginInfo) oi.readObject();
+                        String s1 = AdminLogin(adminLogin);
+                        os.writeUTF(s1);
+                        os.flush();
+                        break;
 
                 }
-                
-                
-            }catch (Exception e){
+
+            } catch (Exception e) {
                 e.printStackTrace();
                 return;
             }
         }
-        
+
     }
 }
