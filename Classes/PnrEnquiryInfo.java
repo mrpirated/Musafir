4c07b3b4f7b8b35f26ba5297df058274139c5cdb
Aditya Darji <darjiaditya2000@gmail.com>
2020-10-31 20:48:20 +0530

    PNR enquiry done.

--- Classes/PnrEnquiryInfo.java
+++ Classes/PnrEnquiryInfo.java
@@ -1,13 +1,53 @@
 package Classes;
 
-public class PnrEnquiryInfo {
-    private String pnr;
+import java.io.*;
 
-    public PnrEnquiryInfo(String pnr) {
-        this.pnr = pnr;
+public class PnrEnquiryInfo implements Serializable {
+    private String name, coach_no, gender;
+    private Integer type, seat_no, waiting, age;
+    private char meal;
+
+    public PnrEnquiryInfo(String name, Integer type, String coach_no, Integer seat_no, Integer waiting, Integer age,
+            String gender, char meal) {
+        this.name = name;
+        this.type = type;
+        this.coach_no = coach_no;
+        this.seat_no = seat_no;
+        this.waiting = waiting;
+        this.age = age;
+        this.gender = gender;
+        this.meal = meal;
     }
 
-    public String getPnr() {
-        return pnr;
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getCoach_no() {
+        return coach_no;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getSeat_no() {
+        return seat_no;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public Integer getWaiting() {
+        return waiting;
+    }
+
+    public char getMeal() {
+        return meal;
     }
 }
